36078d5de0020d3c0e2529b50200c12400e7641b
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-05-24 16:56:33 +0000

    wokoakro

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Tim Brady All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Tim Brady
+// Created on: june 2023
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function updates the slider value.
+ */
+function myButtonClicked() {
+  // input
+  const numberGuessed = parseInt(
+    document.getElementById("guessed-number").value
+  )
+
+  // process
+  if (numberGuessed == randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was, " + randomNumber + "!" + "You got it! Good job."
+  }
+
+  // block of code to be executed if condition1 is true
+  if (numberGuessed != randomNumber) {
+    document.getElementById("answer").innerHTML =
+      "The answer was, " + randomNumber + "!" + "Nice guess, but try again."
+  }
+}
